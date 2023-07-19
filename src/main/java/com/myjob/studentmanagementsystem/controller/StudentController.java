620bd7d9ff3a7266c02cf53023845a1cf1333fc0
OmerCeleb <omer534@outlook.com>
2023-07-19 23:17:15 +0200

    Student-management-system

--- src/main/java/com/myjob/studentmanagementsystem/controller/StudentController.java
+++ src/main/java/com/myjob/studentmanagementsystem/controller/StudentController.java
@@ -1,9 +1,12 @@
 package com.myjob.studentmanagementsystem.controller;
 
+import com.myjob.studentmanagementsystem.entity.Student;
 import com.myjob.studentmanagementsystem.service.StudentService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class StudentController {
@@ -21,5 +24,20 @@ public class StudentController {
         model.addAttribute("students", studentService.getAllStudent());
 
         return "students";
+    }
+
+    @GetMapping("/students/new")
+    public String createStudentForm(Model model) {
+        //create student object to hold student form data
+        Student student = new Student();
+        model.addAttribute("student", student);
+
+        return "create_student";
+
+    }
+
+    @PostMapping("/students")
+    public String saveStudent(@ModelAttribute("student") Student student) {
+
     }
 }
